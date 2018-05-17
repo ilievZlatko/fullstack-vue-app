f039ec4d745c5433d1c9d3e6611c7fa91bdfe2e6
Zlatko Iliev <iliev.zlatko@gmail.com>
2018-05-17 14:10:06 +0300

    setup the register page

--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,23 @@
-console.log('hello')
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
+// Initialize app
+const app = express()
+const PORT = process.env.PORT || 8081
+
+// Apply Middleware
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(cors())
+
+// Routes
+app.post('/register', (req, res, next) => {
+  res.send({
+    message: `Hello ${req.body.email}! Your user was registered!`
+  })
+})
+
+// Start server
+app.listen(PORT)
